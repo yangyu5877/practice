20d5612712bb50a89d27e5af2c0785976d60a924
yangyou <yangyou@wacai.com>
2018-10-09 10:42:13 +0800

    yangmap

--- src/yangmap/YangMap.java
+++ src/yangmap/YangMap.java
@@ -1,11 +1,47 @@
 package yangmap;
 
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Created by yangyou on 2018/10/8.
  */
-public interface YangMap {
+public interface YangMap<K, V> {
 
-    // TODO: 2018/10/8 手撕hashmap源代码
+    // 一个key只能对应一个value， key不能重复， key可以为null
+
+    int size();
+
+    boolean isEmpty();
+
+    boolean containsKey(Object key);
+
+    boolean containsValue(Object value);
+
+    V get(Object key);
+
+    //返回的值是前一个与key值mapping的value， 如果没有，则返回null
+    V put(K key, V value);
+
+    V remove(Object key);
+
+    void putAll(Map<? extends K, ? extends V> m);
+
+    void clear();
+
+    //   三个View视图， 包含key set， value collection， entry set
+
+    Set<K> keySet();
+
+    Collection<V> values();
+
+
+
+
+
+
+
 
 
 }
